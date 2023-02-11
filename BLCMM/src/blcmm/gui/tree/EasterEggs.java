f9e03c31f4278474ac8b6df76014b17811ecdb74
CJ Kucera <cj@apocalyptech.com>
2023-02-11 16:13:46 -0600

    Adding GPL exceptions for the BLCMM components that are still closed-source

--- BLCMM/src/blcmm/gui/tree/EasterEggs.java
+++ BLCMM/src/blcmm/gui/tree/EasterEggs.java
@@ -14,6 +14,15 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>
  *
+ * Additional permission under GNU GPL version 3 section 7:
+ *
+ * If you modify this Program, or any covered work, by linking or
+ * combining it with BLCMM Launcher, BLCMM Lib Distributor, BLCMM
+ * Resources, or BLCMM Utilities (or modified versions of those
+ * libraries), containing parts covered by the terms of their
+ * proprietary license, the licensors of this Program grant you
+ * additional permission to convey the resulting work.
+ *
  */
 package blcmm.gui.tree;
 
