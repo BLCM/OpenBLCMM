dbc57756876f49d00864a58c8b7d28c0d279c1c5
CJ Kucera <cj@apocalyptech.com>
2023-03-05 20:19:26 -0600

    Reimplement blcmm.utilites.HotfixType -- seems just a basic Enum

--- BLCMM/src/blcmm/FileVerifier.java
+++ BLCMM/src/blcmm/FileVerifier.java
@@ -42,7 +42,7 @@ class FileVerifier {
     private static final String[][] FILES_TO_VERIFY = {
         {"BLCMM_Data_Interaction_Library", "e9f94ef10b1ef66f17e626648289a38d760d5dd745cc8d8f80e454cb5c812c45"},
         {"BLCMM_Resources",/*           */ "f40fc4f1c46236f7d914618ca46c8e78858b39601fb444e7f92d7406430b845a"},
-        {"BLCMM_Utilities",/*           */ "43918d8cbdc75b5a693df22dbc9abc2dedc556ef98a99743ea9246a3e89d2446"},};
+        {"BLCMM_Utilities",/*           */ "418dcd24755c6bb7042b02e06ec051bc6ba4201f299849c7d9227b2da4087250"},};
 
     static boolean verifyFiles() {
         boolean missing = false, corrupt = false, changed = false, ex2 = false;
---
+++ BLCMM/src/blcmm/model/HotfixType.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright (C) 2023 CJ Kucera
+ *
+ * BLCMM is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ *
+ * Additional permission under GNU GPL version 3 section 7:
+ *
+ * If you modify this Program, or any covered work, by linking or
+ * combining it with BLCMM Launcher, BLCMM Lib Distributor, BLCMM
+ * Resources, or BLCMM Utilities (or modified versions of those
+ * libraries), containing parts covered by the terms of their
+ * proprietary license, the licensors of this Program grant you
+ * additional permission to convey the resulting work.
+ *
+ */
+
+package blcmm.model;
+
+/**
+ * Enum to describe the hotfix types available for BLCMM.  This class was
+ * reimplemented based on the calls BLCMM makes into BLCMM_Utilities.jar,
+ * without reference to the original sourcecode.
+ * 
+ * @author apocalyptech
+ */
+public enum HotfixType {
+    
+    // Enum members
+    PATCH,
+    LEVEL,
+    ONDEMAND;
+    
+}
