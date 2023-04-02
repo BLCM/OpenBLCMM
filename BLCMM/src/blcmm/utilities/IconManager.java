968dc60be824b02e95130124a173e31acfb88f24
CJ Kucera <cj@apocalyptech.com>
2023-04-02 00:53:28 -0500

    Added image caching to IconManager

--- BLCMM/src/blcmm/utilities/IconManager.java
+++ BLCMM/src/blcmm/utilities/IconManager.java
@@ -32,6 +32,7 @@ import java.awt.Image;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.HashMap;
 import javax.imageio.ImageIO;
 
 /**
@@ -44,6 +45,8 @@ import javax.imageio.ImageIO;
 public class IconManager {
 
     static final String BLCMM_ICON_PATH = "/resources/Icon.png";
+    static final int BLANK_DEFAULT_RES = 256;
+    private final static HashMap<String, HashMap<Integer, Image>> iconCache = new HashMap<> ();
 
     /**
      * Returns the specified icon, or an "empty" image if the path can't be
@@ -53,30 +56,52 @@ public class IconManager {
      * @return The icon for the game
      */
     public static Image getIcon(String path) {
-        try {
-            InputStream stream = IconManager.class.getResourceAsStream(path);
-            if (stream != null) {
-                Image im = ImageIO.read(stream);
-                if (im != null) {
-                    return im;
-                }
-            }
-        } catch (IOException e) {
-            // Whatever, just default to our blank fallback
-        }
-        return new BufferedImage(256, 256, BufferedImage.TYPE_INT_ARGB);
+        return IconManager.getIcon(path, -1);
     }
 
     /**
-     * Returns the specified icon, scaled to the given width/height.
+     * Returns the specified icon, scaled to the given width/height.  If the
+     * size is given as -1, the icon will be returned in its "native"
+     * resolution.
      *
      * @param path The resource path to the icon
      * @param size The size to scale to -- images are assumed to be square
      * @return The icon for the game
      */
     public static Image getIcon(String path, int size) {
-        Image im = getIcon(path);
-        return im.getScaledInstance(size, size, Image.SCALE_SMOOTH);
+
+        // If we haven't seen this path at all, load in its default resolution
+        if (!IconManager.iconCache.containsKey(path)) {
+            Image im = null;
+            try {
+                InputStream stream = IconManager.class.getResourceAsStream(path);
+                if (stream != null) {
+                    im = ImageIO.read(stream);
+                }
+            } catch (IOException e) {
+                // Whatever, just default to our blank fallback
+            }
+            if (im == null) {
+                im = new BufferedImage(IconManager.BLANK_DEFAULT_RES,
+                        IconManager.BLANK_DEFAULT_RES,
+                        BufferedImage.TYPE_INT_ARGB);
+            }
+            IconManager.iconCache.put(path, new HashMap<> ());
+            IconManager.iconCache.get(path).put(-1, im);
+        }
+
+        // If we haven't seen this *size* at all, do a resize.  We know that
+        // -1 is already in here since we populated it above, so we'll never
+        // try to resize to -1x-1.
+        if (!IconManager.iconCache.get(path).containsKey(size)) {
+            IconManager.iconCache.get(path).put(
+                    size,
+                    IconManager.iconCache.get(path).get(-1).getScaledInstance(size, size, Image.SCALE_SMOOTH)
+            );
+        }
+
+        // If we got here, we should have *something*, even if it's just blank.
+        return IconManager.iconCache.get(path).get(size);
     }
 
     /**
