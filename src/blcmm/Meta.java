e0530126556016f1df3f552e33ebf94cde33453e
CJ Kucera <cj@apocalyptech.com>
2023-04-13 15:56:03 -0500

    Note about semantic versioning conventions

--- src/blcmm/Meta.java
+++ src/blcmm/Meta.java
@@ -42,7 +42,7 @@ public class Meta {
     public static final String NAME = "OpenBLCMM";
 
     /**
-     * App version.
+     * App version.  Should follow https://semver.org/ conventions.
      */
     public static final String VERSION = "1.3.0-beta.3";
 
