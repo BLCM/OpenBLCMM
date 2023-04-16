a64c57cb1b61d0a25beda5b56a039de30da83756
CJ Kucera <cj@apocalyptech.com>
2023-04-16 00:07:16 -0500

    Completely untested, but making the sections a little more clear

--- windows-processing/native-compile.bat
+++ windows-processing/native-compile.bat
@@ -4,24 +4,39 @@ Rem Original commandline, prior to pulling that config into the jarfile
 Rem native-image -Djava.awt.headless=false -H:ReflectionConfigurationFiles=conf-dir/reflect-config.json -H:ResourceConfigurationFiles=conf-dir/resource-config.json -H:JNIConfigurationFiles=conf-dir/jni-config.json -jar OpenBLCMM.jar
 
 echo Compiling using native-image
+echo ----------------------------
+echo.
 pushd ..\store
 call native-image -jar OpenBLCMM.jar
+echo.
 
 echo Setting icon on EXE
+echo -------------------
+echo.
 RCEDIT64.exe /I OpenBLCMM.exe ..\windows-processing\openblcmm.ico
+echo.
 
 echo Setting to Windows Subsystem (avoids cmd.exe-window spawning)
+echo -------------------------------------------------------------
+echo.
 Rem See https://github.com/oracle/graal/issues/2256
 EDITBIN /SUBSYSTEM:WINDOWS OpenBLCMM.exe
+echo.
 
 echo Collecting into subdir
+echo ----------------------
+echo.
 rd compiled /s /q
 md compiled
 copy *.dll compiled
 copy OpenBLCMM.exe compiled
+echo.
 
 echo Reporting
+echo ---------
+echo.
 dir compiled
+echo.
 
 popd
 
