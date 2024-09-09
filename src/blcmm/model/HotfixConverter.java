ecee5cbfbdbf9fadd7d38d6b86f8b669cb0dd19e
CJ Kucera <cj@apocalyptech.com>
2024-09-09 15:28:37 -0500

    Remove GPLv3 sec 9 extra clause on license headers
    Got permission from LightChaosman awhile ago to remove these clauses,
    which permit linking OpenBLCMM to the original (still-not-opensource)
    extra libraries which used to compose the entire app.  So, we've got
    a squeaky-clean GPLv3 now.  Thanks again, Light!

--- src/blcmm/model/HotfixConverter.java
+++ src/blcmm/model/HotfixConverter.java
@@ -16,16 +16,6 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>
  *
- * Additional permission under GNU GPL version 3 section 7:
- *
- * If you modify this Program, or any covered work, by linking or
- * combining it with the original proprietary BLCMM Launcher, BLCMM
- * Lib Distributor, BLCMM Utilities, or BLCMM Data Interaction Library
- * Jarfiles (or modified versions of those libraries), containing parts
- * covered by the terms of their proprietary license, the licensors of
- * this Program grant you additional permission to convey the resulting
- * work.
- *
  */
 
 package blcmm.model;
